b9ce17d14d1c9a3be28894fc4b7c3b9deaa4ae6b
Булатова Дарья <bulatovadasha2005@gmail.com>
2025-06-09 19:51:33 +0500

    Итоговый

--- src/view/error-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../framework/view/abstract-view';
-
-const createErrorTemplate = () => '<p class="trip-events__msg">Failed to load latest route information</p>';
-
-export default class ErrorView extends AbstractView {
-  get template() {
-    return createErrorTemplate();
-  }
-}
--- src/view/load-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../framework/view/abstract-view';
-
-const createLoadTemplate = () => '<p class="trip-events__msg">Loading...</p>';
-
-export default class LoadView extends AbstractView {
-  get template() {
-    return createLoadTemplate();
-  }
-}
